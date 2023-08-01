5fa9c24a2f8416b42f67ba4fbff17c29f67ac618
Mario Alvarado <mario.alva.angu@gmail.com>
2023-08-01 14:50:02 -0600

    Testing auto deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDtTUG7ZaugX5h-rfVf6PyGW_cXa42eYP0",
+  authDomain: "deploy-test-gcp.firebaseapp.com",
+  projectId: "deploy-test-gcp",
+  storageBucket: "deploy-test-gcp.appspot.com",
+  messagingSenderId: "602140575791",
+  appId: "1:602140575791:web:1d9bf2ba4f20155afededa",
+  measurementId: "G-GMC3H29QZY"
+};
